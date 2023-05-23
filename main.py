e06e8537977e1f5de69ebac0d43c5ca6875d31d3
am-tropin <94761432+am-tropin@users.noreply.github.com>
2023-05-24 00:11:07 +0400

    commented range func

--- main.py
+++ main.py
@@ -38,16 +38,16 @@ def form_post(request: Request, date: str = Form(...)):
 
 # for range
 
-@app.get("/{date1}_{date2}")
-async def get_anniv_for_range(date1: str, date2: str):
-    return {"Anniversaries in the range:": range_calendar(date1, date2)}
+# @app.get("/{date1}_{date2}")
+# async def get_anniv_for_range(date1: str, date2: str):
+#     return {"Anniversaries in the range:": range_calendar(date1, date2)}
 
-@app.get("/date1_date2/{form_range}")
-def form_post_range(request: Request):
-    result = "Write dates as YYYY-MM-DD"
-    return templates.TemplateResponse('form_range.html', context={'request': request, 'result': result})
+# @app.get("/date1_date2/{form_range}")
+# def form_post_range(request: Request):
+#     result = "Write dates as YYYY-MM-DD"
+#     return templates.TemplateResponse('form_range.html', context={'request': request, 'result': result})
 
-@app.post("/date1_date2/{form_range}")
-def form_post_range(request: Request, date1: str = Form(...), date2: str = Form(...)):
-    result = range_calendar(date1, date2)
-    return templates.TemplateResponse('form_range.html', context={'request': request, 'result': result.to_html()})
+# @app.post("/date1_date2/{form_range}")
+# def form_post_range(request: Request, date1: str = Form(...), date2: str = Form(...)):
+#     result = range_calendar(date1, date2)
+#     return templates.TemplateResponse('form_range.html', context={'request': request, 'result': result.to_html()})
