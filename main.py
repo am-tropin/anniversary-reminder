ab1c5dff8fd6105f667c67b1422f8e0fec6010b3
am-tropin <94761432+am-tropin@users.noreply.github.com>
2023-05-23 11:55:00 +0400

    commented other

--- main.py
+++ main.py
@@ -22,14 +22,14 @@ templates = Jinja2Templates(directory="templates/")
 async def root():
     return "Welcome to the Anniversary Reminder!"
 
-# @app.get("/{date}")
-# async def get_anniv(date: str):
-#     return {"Age counter:": age_counter_2(date)}
+@app.get("/{date}")
+async def get_anniv(date: str):
+    return {"Age counter:": age_counter_2(date)}
 
-@app.get("/date/{form}")
-def form_post(request: Request):
-    result = "Write a date in format YYYY-MM-DD"
-    return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
+# @app.get("/date/{form}")
+# def form_post(request: Request):
+#     result = "Write a date in format YYYY-MM-DD"
+#     return templates.TemplateResponse('form.html', context={'request': request, 'result': result})
 
 @app.post("/date/{form}")
 def form_post(request: Request, date: str = Form(...)):
